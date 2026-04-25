a708a459f35ed6efbc4e0cfc8d24455063a37d31
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-04-25 18:02:28 +0530

    Add src/convex/users.ts

---
+++ src/convex/users.ts
@@ -0,0 +1,17 @@
+import { getAuthUserId } from "@convex-dev/auth/server";
+import { query, QueryCtx } from "./_generated/server";
+
+export const currentUser = query({
+  args: {},
+  handler: async (ctx) => {
+    const user = await getCurrentUser(ctx);
+    if (user === null) return null;
+    return user;
+  },
+});
+
+export const getCurrentUser = async (ctx: QueryCtx) => {
+  const userId = await getAuthUserId(ctx);
+  if (userId === null) return null;
+  return await ctx.db.get(userId);
+};
